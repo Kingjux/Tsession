e2609ff343719d6daa5c04cde703268632106042
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-05-31 08:43:23 +0300

    Create pair.js

---
+++ pair.js
@@ -0,0 +1,99 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Venocyber_Tech,    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function MANJIRO_SANO_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_Cod3Uchiha_Tech = Cod3Uchiha_Tech({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "", ""]
+             });
+             if(!Pair_Code_By_Cod3Uchiha_Tech.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Cod3Uchiha_Tech.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Cod3Uchiha_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Cod3Uchiha_Tech.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Cod3Uchiha_Tech.sendMessage(Pair_Code_By_Cod3Uchiha_Tech.user.id, { text: '' + b64data });
+
+               let MANJIRO_SANO_MD_TEXT = `
+*_Pair Code Connected by Cod3Uchiha Tech_*
+*_Made With 🤍_*
+______________________________________
+╔════◇
+║ *『 WOW YOU CHOOSEN MANJIRO-SANO-MD 』*
+║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+╚══════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Ytube:* _youtube.com/@Cod3Uchiha_
+║❒ *Owner:* _https://wa.me/message/5OMZ4WFADTV3L1_
+║❒ *Repo:* _https://github.com/Cod3Uchiha/Manjiro-Sano-md_
+║❒ *WaGroup:* _https://chat.whatsapp.com/HSln3blDuuuKvC8njxyCCN_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029Vaf4fUfKWEKpiBnBm93M_
+║❒ *Plugins:* _https://github.com/Cod3Uchiha/Manjiro-Sano-md-plugins_
+╚══════════════════════╝ 
+_____________________________________
+
+_Don't Forget To Give Star To My Repo_`
+ await Pair_Code_By_Cod3Uchiha_Tech.sendMessage(Pair_Code_By_Cod3Uchiha_Tech.user.id,{text:MANJIRO_SANO_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_Cod3Uchiha_Tech.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    MANJIRO_SANO_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await MANJIRO_SANO_MD_PAIR_CODE()
+});
+module.exports = router
